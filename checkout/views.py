06c676cf4bd183be42da23508c6debe4b147c70e
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-06-25 13:56:05 +0000

    creating checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
 
 def checkout(request):
-    """ A view to return the checkout page """
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
 
-    return render(request, 'checkout.html')
+    order_form = OrderForm()
+    template = 'checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
--- mr_kris_shop/settings.py
+++ mr_kris_shop/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'shop',
     'checkout',
     'bag',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mr_kris_shop.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -85,6 +88,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
