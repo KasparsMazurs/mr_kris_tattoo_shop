5fd9a0c410d4d2ccdecdc605395c76a997e2a054
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-06-25 09:53:12 +0000

    Implement feature for removing items from the bag and updating the item quantities in the bag.

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect, HttpResponse
-from django.views.decorators.http import require_POST
-from django.http import JsonResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse
+
 
 # Create your views here.
 def bag(request):
@@ -23,3 +22,25 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('bag'))
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    bag = request.session.get('bag', {})  # Retrieve the bag from the session
+    bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
