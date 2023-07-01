58fa54ce04e5108b9bd5b3d34042b3d8fcceb85d
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-07-01 14:45:58 +0000

    Finalizing stripe implementation

--- mr_kris_shop/settings.py
+++ mr_kris_shop/settings.py
@@ -207,4 +207,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
-# STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
