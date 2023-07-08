1c97afa4628d3dcdf22f644fe8563d2dfba9b0eb
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-07-08 06:10:38 +0000

    fix typo in debuging

--- mr_kris_shop/settings.py
+++ mr_kris_shop/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = falt
+DEBUG = False
 
 ALLOWED_HOSTS = ["mr-kris-tattoo-shop.herokuapp.com", "localhost",
                  "127.0.0.1:8000", "127.0.0.1"]
