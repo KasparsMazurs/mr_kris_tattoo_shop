301cc7def62e0883802f7c050616ee1b1653cfc8
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-07-08 06:45:41 +0000

    connecting s3

--- mr_kris_shop/settings.py
+++ mr_kris_shop/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ["mr-kris-tattoo-shop.herokuapp.com", "localhost",
                  "127.0.0.1:8000", "127.0.0.1"]
